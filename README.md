1494540431ca28823d93aa163e3892bd8ca8df85
Alec Fong <a9fong@ucsd.edu>
2025-05-20 01:36:09 -0700

    feat: completed explore section

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab8-Starter
+
+Graceful degradation and Service Workers are related in that we start with "max technology", as in the browser runs with full access to the network, and we "address the lower levels" with Service Workers. Service workers essentially handle the cases where we can't access the network, like when we are offline and without internet. Service Workers store request response pairs in the browser, reducing reliance on the network and gracefully handling the "lower levels".
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -46,14 +46,19 @@ function initializeServiceWorker() {
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
   // B2. TODO - Listen for the 'load' event on the window object.
+  window.addEventListener('load', async () => {
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
+    const registration = await navigator.serviceWorker.register('/sw.js', { scope: '/' });
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
+    if(registration.active) console.log('Registration successful');
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
+    else console.log('Registration failed');
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  });
 }
 
 /**
@@ -73,7 +78,7 @@ async function getRecipes() {
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
-  let recipes = [];
+  const recipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
@@ -95,11 +100,11 @@ async function getRecipes() {
   //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
   //            you must either use "await fetch(...)" or "fetch.then(...)". This
   //            function is using the async keyword so we recommend "await"
-        let response = await fetch(RECIPE_URLS[i]);
+        const response = await fetch(RECIPE_URLS[i]);
   // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
   //            NOTE: .json() is ALSO asynchronous, so you will need to use
   //            "await" again
-        let response_json = await response.json();
+        const response_json = await response.json();
   // A8. TODO - Add the new recipe to the recipes array
         recipes.push(response_json);
   // A9. TODO - Check to see if you have finished retrieving all of the recipes,
